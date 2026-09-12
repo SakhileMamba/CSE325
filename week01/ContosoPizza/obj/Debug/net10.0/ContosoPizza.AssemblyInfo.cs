a2a518ed52386648a80604348b8e8f173298636a
Sakhile Mamba <sakhymamba@gmail.com>
2026-09-13 01:39:53 +0200

    Add API request examples

--- week01/ContosoPizza/obj/Debug/net10.0/ContosoPizza.AssemblyInfo.cs
+++ week01/ContosoPizza/obj/Debug/net10.0/ContosoPizza.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ContosoPizza")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7ddce2ebf9fc0d375edfe0e92daca2a117bdea5e")]
 [assembly: System.Reflection.AssemblyProductAttribute("ContosoPizza")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ContosoPizza")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
